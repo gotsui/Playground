95af2890fd9d6ce0e03b9d276669a13093e67022
gotsui <krhamui.2016@gmail.com>
2026-06-11 00:08:01 +0900

    WBSのダイアログのスクロール修正

--- projects/node/src/app/wbs/_components/modal/Dialog.tsx
+++ projects/node/src/app/wbs/_components/modal/Dialog.tsx
@@ -50,7 +50,7 @@ const Dialog = ({
                             <X />
                         </button>
                     </div>
-                    <div className="flex-1">
+                    <div className="flex-1 min-h-0">
                         {children}
                     </div>
                 </div>
