62fc78d461d660283d131486c68be757050a053b
gotsui <krhamui.2016@gmail.com>
2026-06-13 08:57:14 +0900

    配列を条件の一致不一致で分割する関数、配列からnull, undefinedを削除する関数を作成

--- projects/node/src/lib/array.ts
+++ projects/node/src/lib/array.ts
@@ -10,3 +10,20 @@ export const range: Range = (begin: number, end?: number) => {
         return [...Array(end - begin)].map((_, i) => (begin + i));
     }
 };
+
+export const partition = <T>(
+  array: T[],
+  predicate: (item: T) => boolean,
+): [T[], T[]] => {
+    return array.reduce<[T[], T[]]>(
+        (acc, item) => {
+            acc[predicate(item) ? 0 : 1].push(item);
+            return acc;
+        },
+        [[], []]
+    );
+};
+
+export const trim = <T>(array: (T | null | undefined)[]): T[] => {
+    return array.filter((item): item is Exclude<typeof item, null | undefined> => item != null);
+};
